86eb21f587226294bc9b1c7a5a6f5aea8d7666d5
janet.xiushi <eupirate@gmail.com>
2022-07-17 15:21:26 +0100

    day4_nested_list_treasure_map

---
+++ day4_nesting_list_treasure_maps.py
@@ -0,0 +1,35 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+#***********************************
+''' version 1
+position_x = int(position[0])
+position_y = int(position[1])
+
+
+selected_row = map[position_y - 1]
+selected_row[position_x -1] = "X"
+#***********************************
+'''
+#version 2
+position_x = int(position[0])
+position_y = int(position[1])
+
+locate_x = map[position_x - 1]
+locate_x[position_y - 1] = "X"
+
+
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+
+print(f"{row1}\n{row2}\n{row3}")
