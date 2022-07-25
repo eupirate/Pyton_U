c6bf1563932c498bfca3c900f5ef262ec02f198e
janet.xiushi <eupirate@gmail.com>
2022-07-25 21:51:47 +0100

    Update average_height.py

--- day5/average_height.py
+++ day5/average_height.py
@@ -9,21 +9,21 @@ for n in range(0, len(student_heights)):
 #Write your code below this row 👇
 
 # [180 124 165 173 189 169 146]
-# 156 178 165 171 187
+# [156 178 165 171 187]
 
 total = 0
 for height in student_heights:
     total += height
-#print(f"The total height is {total}")
+print(f"The total height is {total}")
 
 num_student = 0
 for student in student_heights:
     num_student += 1
-#print(f"The total student is {num_student}")
+print(f"The total student is {num_student}")
 
 # avg_height = round(total / len(student_heights))
 # print(f"The average height is {avg_height}")
 
 avg_height = round(total / num_student)
-#print(f"The average height is {avg_height}")
+print(f"The average height is {avg_height}")
 print(avg_height)
